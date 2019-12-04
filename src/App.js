1eec55e394c1c3c20275d61ec883e87c6336efa9
ori <oritsadok1@gmail.com>
2019-12-04 11:21:06 +0200

    finish-setup

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { observer } from 'mobx-react'
 
-function App() {
+@observer
+class App extends Component {
+  render() {
   return (
     <div className="App">
       <header className="App-header">
@@ -22,5 +25,5 @@ function App() {
     </div>
   );
 }
-
+}
 export default App;
